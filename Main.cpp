11847850b2b42ca8c4e229a27ae3a017dfd39216
Angel-Arevalo <anarevalof@unal.edu.co>
2024-06-29 11:54:36 -0500

    terminanda la clase string

--- Main.cpp
+++ Main.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-    String x("HELLO");
-    String y("WORLD");
+    String x = "HELLO";
+    String y("hello");
+    bool m = x /= y;
+    std::cout << m << "\n\n";
     std::cout << x.getlength() << "\n\n";
     std::cout << x << "\n\n";
     std::cout << y.getlength() << "\n\n";
     std::cout << y << "\n\n";
-    x += "world";
-    std::cout << x.getlength() << "\n\n";
-    std::cout << x.toLowerCase() << "\n\n";
+    std::cout << (x + y)<< "\n\n";
     return 0;
 }
--- String.cpp
+++ String.cpp
@@ -31,6 +31,22 @@ class String {
 		    setString(y);
 		}
 
+		String concatenateSum(String args) {
+		    int length = this->length + args.getlength() + 1;
+		    const char* argsString = args.getString();
+		    char* StringCont = new char[length];
+		    int i, k = 0;
+
+		    for (i = 0; i < this->length; ++i) {
+                StringCont[i] = this->cString[i];
+		    }for (int j = 0; j < args.getlength(); ++j, ++i) {
+                StringCont[i] = argsString[j];
+            }
+		    StringCont[length - 1] = '\0';
+            String y(StringCont);
+		    return y;
+		}
+
 		void setString(String args) {
             delete[] this->cString;
             // change the length
@@ -43,11 +59,21 @@ class String {
                 this->cString[i] = argsString[i];
             }
             this->cString[this->length] = '\0';
-            std::cout << this->cString << std::endl;
+        }
+
+        void setString(const char* args) {
+            String argsObject(args);
+            this->setString(argsObject);
         }
 
         bool compareStrings(String args) {
-
+            if (this->length == args.getlength()) {
+                const char* argsString = args.getString();
+                for (int i = 0; i < this->length; i++) {
+                    if (this->cString[i] != argsString[i])
+                        return false;
+                }
+            }else return false;
             return true;
         }
 
@@ -63,6 +89,17 @@ class String {
 
             this->cString[this->length] = '\0';
 		};
+
+		String(const char* string, int length) {
+		    delete[] this->cString;
+            this->length = length;
+            this->cString = new char[length + 1];
+
+            for (int i = 0; i< length; i++) {
+                cString[i] = string[i];
+            }
+            cString[length] = '\0';
+		}
 
 		// a getter to use in the methods
 		int getlength() {
@@ -90,12 +127,22 @@ class String {
             return lower;
 		}
 
-		// print a object is equal to print a string
-		friend std::ostream& operator<<(std::ostream& os, const String& str) {
-            os << str.cString;
-            return os;
-        }
-
+		String toUpperCase() {
+		    int index;
+		    char* StringUpper = new char[this->length + 1];
+            for (int i = 0; i < this->length; i++) {
+                index = this->cString[i];
+                if (97 <= index && index < 124) {
+                    char x = index - 32;
+                    StringUpper[i] = x;
+                }else if (this->cString[i] != '\0') {
+                    StringUpper[i] = this->cString[i];
+                }
+            }
+            StringUpper[this->length] = '\0';
+            String Upper(StringUpper);
+            return Upper;
+		}
         // override the method of the operator +=
         String& operator+= (String args) {
             this->concatenate(args);
@@ -103,7 +150,51 @@ class String {
         }
         String& operator+= (const char* args) {
             String str(args);
-            this->concatenate(str);
+            this->concatenateSum(str);
             return *this;
+        }
+        // override the operator +
+        String operator+ (String args) {
+            return this->concatenateSum(args);
+        }
+        String operator+ (const char* args) {
+            String str(args);
+            return this->concatenateSum(str);
+        }
+
+        // override the operator == and ===
+        bool operator== (String args) {
+            return this->compareStrings(args);
+        }
+        bool operator== (const char* args) {
+            String argsObject(args);
+            return this->compareStrings(argsObject);
+        }
+        // this is the equalIgnoreCase
+        bool operator/= (String& args) {
+            String x = this->toLowerCase();
+            String y = args.toLowerCase();
+            return (x == y);
+        }
+        bool operator/= (const char* args) {
+            String x = this->toLowerCase();
+            String y(args);
+            return (x == y.toLowerCase());
+        }
+        // override the operator =
+        String& operator=(String other) {
+            if (!(this == &other)) {
+                this->setString(other.cString);
+            }
+            return *this;
+        }
+        void operator= (const char* args) {
+            this->setString(args);
+        }
+
+        // print a object is equal to print a string
+		friend std::ostream& operator<<(std::ostream& os, const String& str) {
+            os << str.cString;
+            return os;
         }
 };
